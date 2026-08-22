f0fcd36faeda3ffff6abc2e0b6f683e3a16e06e1
Tirefly <tzlUltraGames@outlook.com>
2026-08-23 01:44:32 +0800

    【DEL】删除对 UnrealSharp 的支持

--- Source/TireflyCombatSystem/TireflyCombatSystem.Build.cs
+++ Source/TireflyCombatSystem/TireflyCombatSystem.Build.cs
@@ -33,8 +33,6 @@ public class TireflyCombatSystem : ModuleRules
 				"StateTreeModule",
 				"GameplayStateTreeModule",
 				"DeveloperSettings",
-				"GameplayMessageRuntime",
-				"TireflyObjectPool"
 			}
 			);
 			
